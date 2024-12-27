5dc1b986ff71afea66cf5d7ff5ec94239d8fbc9e
devil <devil@DESKTOP-JF4OGC2>
2024-12-27 22:45:01 +0200

    Add project files.

---
+++ ApiAggregator.Core/ApiAggregator.cs
@@ -0,0 +1,32 @@
+﻿namespace ApiAggregator.Core;
+
+public interface IExternalApi
+{
+    IList<IApiFunction> Functions { get; }
+}
+
+public interface IApiFunction
+{
+    string Name { get; }
+
+    string Pattern { get; }
+}
+
+public interface IAggregationFunction
+{
+    string Name { get; }
+
+    IList<IApiFunction> Functions { get; }
+
+    Task<IList<Task<IApiResult>>> ToTasks();
+}
+
+public interface IInternalApi
+{
+    IList<IAggregationFunction> AggFunctions { get; }
+}
+
+public interface IApiResult
+{
+
+}
---
+++ ApiAggregator.Core/Engine.cs
@@ -0,0 +1,11 @@
+﻿namespace ApiAggregator.Core;
+
+internal class Engine
+{
+    public static async Task<IList<IApiResult>> Run(IAggregationFunction aggFunction, CancellationToken ct)
+    {
+        var callTasks = await aggFunction.ToTasks();
+        Task.WaitAll([.. callTasks], ct);
+        return callTasks.Select(t => t.Result).ToList();
+    }
+}
